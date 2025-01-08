2c087d9ca9c11e19f1abae3db87a9b20b727c80a
Kusuma0112 <kirankusuma0112@gmail.com>
2025-01-08 17:46:01 +0530

    if-else statements

--- p13.py
+++ p13.py
@@ -1,7 +1,15 @@
-a =int(input("Enter your age"))
+a = int(input("Enter your age"))
 print("Your age is:",a)
-
+# Conditional operators
+# >, <,==,>=,<=,!=
+print(a>18)
+print(a<=18)
+print(a==18)
+print(a!=18)
 if(a>18):
-    print("you can drive")
+    print("You can drive")
+    print("Yes")
 else:
     print("You cannot drive")
+    print("No")
+    print("Yay!")
