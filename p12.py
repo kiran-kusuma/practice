1406a46a961afb29dc47d15ec522fe169b1b5271
Kusuma0112 <kirankusuma0112@gmail.com>
2025-01-08 17:06:42 +0530

    strings title

--- p12.py
+++ p12.py
@@ -1,11 +1,11 @@
 # strings are immutable
-a ="Thanvi!!!!!!!!!!! Thanvi"
+a ="!!Harry !!!!!!!!!!!! Harry"
 print(len(a))
 print(a)
 print(a.upper())
 print(a.lower())
 print(a.rstrip("!") )
-print(a.replace("Thanvi","jyoshu"))  
+print(a.replace("Harry","jyoshu"))  
 print(a.split(" ")) 
 blogHeading = "introdution to js "
 print(blogHeading.capitalize())
@@ -13,15 +13,47 @@ print(blogHeading.capitalize())
 str1 = "welcom to the console!!!!"
 print(len(str1)) 
 print(len(str1.center(50)))
-print(a.count("Thanvi"))
+print(a.count("Harry"))
 
 str1 ="Welcom to the console !!!"
-print (str1.endswith("!!!"))
+print(str1.endswith("!!!"))
 
 str1 ="Welcom to the console !!!"
-print (str1.endswith("to",4,10))
+print(str1.endswith("to", 4, 10))
 
 srt1 = "He's name is Dan. He is an honest man."
-print(str1.find("is"))
+print(str1.find("ishh"))
+# print(str1.index("ishh"))
+
+
+str1 ="Welcom to the console"
+print(str1.isalnum())
+str1 = "welcom"
+print(str1.isalpha())
+
+str1 = "hello world"
+print(str1.islower())
+ 
+str1 =" We wish you a merry christmas\n"
+print(str1.isprintable())
+str1 = "      " #using spacebar
+print(str1.isspace())
+str2 ="        "     #using Tab
+print(str2.isspace())
+
+str1 ="World Health  Organization"
+print(str1.istitle())
+
+str2 ="To Kill a Mocking bird"
+print(str2.istitle())
+
+str1 ="Python is a Interpreted Language"
+print(str1.startswith("Python"))
+
+str1 = "Python is a Interpreted Language"
+print(str1.swapcase())
+
+str1 ="His name is Don. Don is an honest man."
+print(str1.title())
 
 
