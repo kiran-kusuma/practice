785a6b0e8b7462c18b46aea528cc65cffe1aeede
Kusuma0112 <kirankusuma0112@gmail.com>
2025-01-08 18:25:16 +0530

    if-else conditinal

---
+++ p15.py
@@ -0,0 +1,11 @@
+num = int(input("Enter the  value of num: "))
+if( num < 0 ):
+    print("number is negative.")
+elif ( num == 0):
+    print("number is Zero.")
+elif (num == 999):
+    print("number is Special.")
+else:
+    print("number is positive.")
+        
+
