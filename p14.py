4497d2f0ec8809f34afab557414447e9fb2b4e0b
Kusuma0112 <kirankusuma0112@gmail.com>
2025-01-08 18:06:29 +0530

    if else statement

---
+++ p14.py
@@ -0,0 +1,8 @@
+appleprice = 10
+budget = 200
+if ( budget - appleprice > 50):
+    print("Alexa, add 1 kg Apples to the cart.")
+elif( budget - appleprice > 70):
+    print("Its okay you can buy")
+else:
+    print("Alexa, do not add Apple to the cart.")
