1655d9583537799049ea5ca5c7b106dc425dc750
Kusuma0112 <kirankusuma0112@gmail.com>
2025-01-09 14:46:58 +0530

    match case

---
+++ p20.py
@@ -0,0 +1,17 @@
+X = 4
+# X is the variable to match
+match X:
+    # if X is )
+    case 0:
+        print(" X is Zero")
+    # case with if-condition
+    case 4:
+        print("case is 4")
+    # Empty  case with  if-condition
+    case _ if X < 10:
+        print("x is < 10")
+    # default case( will only be matched if  the above cases were not matched)
+    # so it is basically just an else:
+    case _:
+        print(X)    
+
