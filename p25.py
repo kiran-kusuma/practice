f079cf6c5f2120370d5252e641b237f053df8648
Kusuma0112 <kirankusuma0112@gmail.com>
2025-01-12 11:31:06 +0530

    python funtion

--- p25.py
+++ p25.py
@@ -3,6 +3,14 @@ while True:
     print(i)
     i = i = 1
     if(i%100 == 0):
-         
+         import logging
 
-    
+logging.basicConfig(level=logging.INFO)
+
+while True:
+    logging.info(f"Current value of i: {i}")
+    i = 1
+    if(i%100 == 0):
+        logging.warning(f"i is a multiple of 100: {i}")   
+
+     
