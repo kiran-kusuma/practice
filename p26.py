92c8fea8b5996ccace8127f9812ea2b1afa442d0
Kusuma0112 <kirankusuma0112@gmail.com>
2025-01-12 11:51:17 +0530

    funtion operation

--- p26.py
+++ p26.py
@@ -1,10 +1,29 @@
+def calculateGmean(a, b):
+    mean =(a*b)/(a*b)
+    print(mean)
+
+
 a = 9
 b = 8
-gmean1 = (a * b)/(a+b)
-print(gmean1)
+if(a>b):
+    print(" First number is greater")
+else:
+    print("Second number is greater or equal")
+calculateGmean(a, b)
+
+
+# gmean1 = (a*b)/(a+b)
+# print(gmean1)
+calculateGmean(a, b)
 
 c = 8
-d = 7
+d = 74
+if(c>d):
+    print(" First number is greater")
+else:
+    print("Second number is greater or equal")
 
-gmean2 = (c * d)/(c+d)
-print(gmean2)
+# gmean2 = (c*d)/(c+d)
+# print(gmean2)
+if(c>d):
+    calculateGmean(c, d)
